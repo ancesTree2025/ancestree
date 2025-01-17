907ef987009c51fcccbb673f8ec98263db004662
s-denisov <67757505+s-denisov@users.noreply.github.com>
2025-01-17 16:20:28 +0000

    build: CI including formatting and linting (#2)
    Co-authored-by: Arsenii Denisov <simon.denisov22@gmail.com>
    Co-authored-by: Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>

---
+++ client/eslint.config.js
@@ -0,0 +1,34 @@
+import pluginJs from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import * as svelteParser from 'svelte-eslint-parser';
+import * as typescriptParser from '@typescript-eslint/parser';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: typescriptParser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.svelte']
+      }
+    },
+    rules: {
+      // note you must disable the base rule
+      // as it can report incorrect errors
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error', // or "warn"
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/recommended']
+];
