293533a6ef3426b1736b3bc370057576ac1f2403
Arsenii Denisov <simon.denisov22@gmail.com>
2025-01-17 12:52:22 +0000

    Format eslint.config.js

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,24 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import * as svelteParser from 'svelte-eslint-parser';
 import * as typescriptParser from '@typescript-eslint/parser';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: {
-      parser: svelteParser,
-      parserOptions: {
-        parser: typescriptParser,
-        project: './tsconfig.json',
-        extraFileExtensions: ['.svelte']
-      }
-    }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...eslintPluginSvelte.configs['flat/recommended'],
+	{
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: typescriptParser,
+				project: './tsconfig.json',
+				extraFileExtensions: ['.svelte']
+			}
+		}
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginSvelte.configs['flat/recommended']
 ];
