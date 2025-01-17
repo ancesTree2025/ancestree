a1ceff1a11500c34ab4d6b6397daa9713ca9527a
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-01-17 14:20:55 +0000

    fixed actions and formatted the code [skip ci]

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,35 +1,34 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tsEslint from 'typescript-eslint';
 import * as svelteParser from 'svelte-eslint-parser';
 import * as typescriptParser from '@typescript-eslint/parser';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		languageOptions: {
-			parser: svelteParser,
-			parserOptions: {
-				parser: typescriptParser,
-				project: './tsconfig.json',
-				extraFileExtensions: ['.svelte']
-			}
-		},
-		rules: {
-			// note you must disable the base rule
-			// as it can report incorrect errors
-			'no-unused-vars': 'off',
-			'@typescript-eslint/no-unused-vars': [
-				'error', // or "warn"
-				{
-					argsIgnorePattern: '^_',
-					caughtErrorsIgnorePattern: '^_'
-				}
-			]
-		}
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	...eslintPluginSvelte.configs['flat/recommended']
+  {
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: typescriptParser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.svelte']
+      }
+    },
+    rules: {
+      // note you must disable the base rule
+      // as it can report incorrect errors
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error', // or "warn"
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/recommended']
 ];
