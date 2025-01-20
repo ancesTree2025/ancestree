9756749d2213ab8e7ceaa3fe4f5ecca72d66962a
mozin751 <123524914+mozin751@users.noreply.github.com>
2025-01-20 18:10:52 +0000

    implement person node

---
+++ client/src/components/Node.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let name: string;
+  export let x: number;
+  export let y: number;
+</script>
+
+<g>
+  <rect class="bg" {x} {y}></rect>
+  <text class="person_name" x={x + 10} y={y + 30}>{name}</text>
+</g>
+
+<style scoped>
+  .person_name {
+    width: 100px;
+    height: 50px;
+    fill: black;
+    stroke: b;
+    rx: 15px;
+    /* font-size: 10px; */
+  }
+
+  .bg {
+    width: 100px;
+    height: 50px;
+    fill: white;
+    stroke: black;
+  }
+</style>
