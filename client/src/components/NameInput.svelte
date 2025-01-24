820ef8c35d13e43b18c3c7df326dffa210d919cf
Arsenii Denisov <simon.denisov22@gmail.com>
2025-01-24 10:41:43 +0000

    Add testing

--- client/src/components/NameInput.svelte
+++ client/src/components/NameInput.svelte
@@ -1,62 +1,17 @@
 <!-- Component to accept a name input from the user -->
 
 <script lang="ts">
-  import type { Marriages, People, Person, PersonID, Tree } from '$lib/familytree/models';
+  import { apiResponseToTree } from '$lib/familytree/fetchTree';
+  import type { Tree } from '$lib/familytree/models';
   import Card from './Card.svelte';
-  // Name input variable (might be useful to set as $state for autocomplete box)
+
   let { treeData = $bindable() }: { treeData?: Tree } = $props<{ treeData?: Tree }>();
 
-  type PersonData = {
-    data: {
-      id: PersonID;
-      name: string;
-      gender: 'male' | 'female';
-    };
-    id: PersonID;
-    depth: number;
-  };
-  type ApiResponse = {
-    root: PersonData;
-    nodes: PersonData[];
-    edges: {
-      node1: PersonID;
-      node2: PersonID;
-    }[];
-  };
   let name = $state('');
   const submitAction = () => {
     fetch(`http://localhost:8080/${name}`).then((response) => {
-      response.json().then((s: ApiResponse) => {
-        // Create map of Person to list of children
-        // Create list of marriages
-        // For each marriage make its children the intersection of the children
-        // of both members
-        const people: Map<PersonID, Person & { depth: number }> = new Map();
-        const marriages: Marriages = [];
-        for (const person of s.nodes.concat(s.root)) {
-          people.set(person.id, { name: person.data.name, x: 0, y: 0, depth: person.depth });
-        }
-        const children = new Map<PersonID, PersonID[]>();
-        for (const edge of s.edges) {
-          // node 1: parent, node 2: child
-          if (people.get(edge.node1)!.depth < people.get(edge.node2)!.depth) {
-            if (!children.has(edge.node1)) {
-              children.set(edge.node2, []);
-            }
-            children.get(edge.node1)?.push(edge.node2);
-          } else {
-            marriages.push({ parents: [edge.node1, edge.node2], children: [] });
-          }
-        }
-        for (const marriage of marriages) {
-          marriage.children = children
-            .get(marriage.parents[0])!
-            .filter((child) => children.get(marriage.parents[1])!.includes(child));
-        }
-        treeData = {
-          people,
-          marriages
-        };
+      response.json().then((s) => {
+        treeData = apiResponseToTree(s);
       });
     });
   };
--- client/src/lib/familytree/fetchTree.ts
+++ client/src/lib/familytree/fetchTree.ts
@@ -1,4 +1,54 @@
-import type { Tree } from './models';
+import type { Marriages, Person, PersonID, Tree } from './models';
+
+type PersonData = {
+  data: {
+    id: PersonID;
+    name: string;
+    gender: 'male' | 'female';
+  };
+  id: PersonID;
+  depth: number;
+};
+type ApiResponse = {
+  root: PersonData;
+  nodes: PersonData[];
+  edges: {
+    node1: PersonID;
+    node2: PersonID;
+  }[];
+};
+export const apiResponseToTree = (res: ApiResponse): Tree => {
+  // Create map of Person to list of children
+  // Create list of marriages
+  // For each marriage make its children the intersection of the children
+  // of both members
+  const people: Map<PersonID, Person & { depth: number }> = new Map();
+  const marriages: Marriages = [];
+  for (const person of res.nodes.concat(res.root)) {
+    people.set(person.id, { name: person.data.name, x: 0, y: 0, depth: person.depth });
+  }
+  const children = new Map<PersonID, PersonID[]>();
+  for (const edge of res.edges) {
+    // node 1: parent, node 2: child
+    if (people.get(edge.node1)!.depth < people.get(edge.node2)!.depth) {
+      if (!children.has(edge.node1)) {
+        children.set(edge.node1, []);
+      }
+      children.get(edge.node1)?.push(edge.node2);
+    } else {
+      marriages.push({ parents: [edge.node1, edge.node2], children: [] });
+    }
+  }
+  for (const marriage of marriages) {
+    marriage.children = children
+      .get(marriage.parents[0])!
+      .filter((child) => children.get(marriage.parents[1])!.includes(child));
+  }
+  return {
+    people,
+    marriages
+  };
+};
 
 export function fetchTree(_: string): Tree {
   // TODO: populate with request
