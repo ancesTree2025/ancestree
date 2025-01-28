c349d9d569785b05ea4afa10dc160e3c3977dc58
jamacz <jack.mccomish@icloud.com>
2025-01-28 13:54:12 +0000

    refactor: move non-tailwind css classes out of layers in app.css

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
   import '../app.css';
-  import '../global.css';
   let { children } = $props();
 </script>
 
 {@render children()}
-
-<style>
-  :global(:root, body, body > div) {
-    height: 100%;
-  }
-</style>
