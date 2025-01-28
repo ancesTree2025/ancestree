8a64ad3250fb6b7d6e9437dc7114de6e640a6830
jamacz <jack.mccomish@icloud.com>
2025-01-28 16:24:07 +0000

    feat: add error message popup on hover

--- client/src/components/NameInput.svelte
+++ client/src/components/NameInput.svelte
@@ -3,7 +3,7 @@
 <script lang="ts">
   import IconSearch from '~icons/tabler/search';
   import IconAlert from '~icons/tabler/alert-triangle-filled';
-  import { fade, scale } from 'svelte/transition';
+  import { scale } from 'svelte/transition';
 
   let {
     nameInput = $bindable(),
@@ -24,7 +24,7 @@
   }
 </script>
 
-<div class="bg-input flex w-80 items-center gap-3 rounded-full pl-4">
+<div class="bg-input relative flex w-80 items-center gap-3 rounded-full pl-4">
   <IconSearch class="text-black opacity-50" />
   <input
     class="flex-1 bg-transparent py-2 outline-none"
@@ -33,13 +33,28 @@
     onkeydown={submitIfEnter}
   />
   {#if loading}
-    <div transition:scale class="mr-3 flex h-5 w-5 items-center">
-      <div class="loader h-5 w-5 bg-black p-1 opacity-50"></div>
+    <div class="absolute right-3 flex h-full w-5 items-center">
+      <div
+        transition:scale={{ duration: 150 }}
+        class="loader h-5 w-5 bg-black p-1 opacity-50"
+      ></div>
     </div>
   {/if}
   {#if error}
-    <div transition:scale class="mr-3 flex h-5 w-5 items-center">
-      <IconAlert class="text-red" />
+    <div class="absolute right-3 flex h-full w-5 items-center">
+      <button
+        transition:scale={{ duration: 150 }}
+        class="text-red peer z-10 transition-transform duration-150 hover:scale-125"
+      >
+        <IconAlert />
+      </button>
+      <div
+        class="absolute bottom-0 left-1/2 flex h-0 w-0 -translate-y-4 flex-col items-center opacity-0 transition-all peer-hover:translate-y-0 peer-hover:opacity-100"
+      >
+        <p class="bg-red mt-2 text-nowrap rounded-xl px-8 py-1 text-center text-sm text-white">
+          {error}
+        </p>
+      </div>
     </div>
   {/if}
 </div>
