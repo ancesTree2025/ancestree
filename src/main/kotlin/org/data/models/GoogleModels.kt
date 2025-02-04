884911b622273668f79d83fc21263d3f22256e5d
mozin751 <123524914+mozin751@users.noreply.github.com>
2025-02-04 21:36:06 +0000

    Made classes nested.

--- src/main/kotlin/org/data/models/GoogleModels.kt
+++ src/main/kotlin/org/data/models/GoogleModels.kt
@@ -3,8 +3,10 @@ package org.data.models
 import kotlinx.serialization.Serializable
 
 /** The classes below are used to extract the information made in Google Knowledge HTTP requests. */
-@Serializable data class ItemListElement(val itemListElement: List<ResultObject>)
-
-@Serializable data class ResultObject(val result: NameObject)
-
-@Serializable data class NameObject(val name: String)
+@Serializable
+data class ItemListElement(val itemListElement: List<ResultObject>) {
+  @Serializable
+  data class ResultObject(val result: NameObject) {
+    @Serializable data class NameObject(val name: String)
+  }
+}
