80b36365ef1cf99121e939cecd2829c9195fb776
Rahul George <rg922@ic.ac.uk>
2025-02-11 21:52:59 +0000

    wip: commented deprecated tests

--- src/test/kotlin/org/integration/RequestParserTest.kt
+++ src/test/kotlin/org/integration/RequestParserTest.kt
@@ -2,55 +2,51 @@ package org.integration
 
 import io.ktor.client.statement.*
 import kotlin.test.*
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
-import org.data.models.WikidataProperties.propertyQIDMapPersonal
-import org.data.parsers.WikiRequestParser
-import org.data.requests.ComplexRequester
 
-class RequestParserTest {
-  private val timeout = 60.seconds
-  private lateinit var response: HttpResponse
 
-  @BeforeTest
-  fun setUp(): Unit = runBlocking {
-    response = ComplexRequester.getLabelAndClaim(listOf("Q317521")) // Elon Musk
-  }
-
-  @Test
-  fun `can parse wikidata request response into QID`() =
-    runTest(timeout = timeout) {
-      val response = ComplexRequester.searchWikidataForQID("Elon Musk")
-      val qid = WikiRequestParser.parseWikidataIDLookup(response)
-      assertEquals("Q317521", qid)
-    }
-
-  @Test
-  fun `can parse wikidata request response into one label and its claims`() =
-    runTest(timeout = timeout) {
-      val labelClaimPair = WikiRequestParser.parseWikidataClaims(response)
-      assert(labelClaimPair.size == 1)
-      assertEquals("Elon Musk", labelClaimPair["Q317521"]?.first)
-      assertNotNull(labelClaimPair["Q317521"]?.second)
-    }
-
-  @Test
-  fun `can parse wikidata request response for personal props`() =
-    runTest(timeout = timeout) {
-      val labelClaimPair = WikiRequestParser.parseWikidataClaims(response, propertyQIDMapPersonal)
-      val claim = labelClaimPair["Q317521"]?.second
-      assertEquals("Elon Musk", labelClaimPair["Q317521"]?.first)
-      for ((_, prop) in propertyQIDMapPersonal) {
-        assert(claim?.contains(prop) ?: false)
-      }
-    }
-
-  @Test
-  fun `can parse wikidata response for location name`() =
-    runTest(timeout = timeout) {
-      val response = ComplexRequester.getLabelAndClaim(listOf("Q189022"))
-      val locInfo = WikiRequestParser.parseWikidataClaims(response, parseClaims = false)
-      assertEquals("Imperial College London", locInfo["Q189022"]?.first)
-    }
-}
+//
+// class RequestParserTest {
+//  private val timeout = 60.seconds
+//  private lateinit var response: HttpResponse
+//
+//  @BeforeTest
+//  fun setUp(): Unit = runBlocking {
+//    response = ComplexRequester.getLabelAndClaim(listOf("Q317521")) // Elon Musk
+//  }
+//
+//  @Test
+//  fun `can parse wikidata request response into QID`() =
+//    runTest(timeout = timeout) {
+//      val response = ComplexRequester.searchWikidataForQID("Elon Musk")
+//      val qid = WikiRequestParser.parseWikidataIDLookup(response)
+//      assertEquals("Q317521", qid)
+//    }
+//
+//  @Test
+//  fun `can parse wikidata request response into one label and its claims`() =
+//    runTest(timeout = timeout) {
+//      val labelClaimPair = WikiRequestParser.parseWikidataClaims(response)
+//      assert(labelClaimPair.size == 1)
+//      assertEquals("Elon Musk", labelClaimPair["Q317521"]?.first)
+//      assertNotNull(labelClaimPair["Q317521"]?.second)
+//    }
+//
+//  @Test
+//  fun `can parse wikidata request response for personal props`() =
+//    runTest(timeout = timeout) {
+//      val labelClaimPair = WikiRequestParser.parseWikidataClaims(response, propertyQIDMapPersonal)
+//      val claim = labelClaimPair["Q317521"]?.second
+//      assertEquals("Elon Musk", labelClaimPair["Q317521"]?.first)
+//      for ((_, prop) in propertyQIDMapPersonal) {
+//        assert(claim?.contains(prop) ?: false)
+//      }
+//    }
+//
+//  @Test
+//  fun `can parse wikidata response for location name`() =
+//    runTest(timeout = timeout) {
+//      val response = ComplexRequester.getLabelAndClaim(listOf("Q189022"))
+//      val locInfo = WikiRequestParser.parseWikidataClaims(response, parseClaims = false)
+//      assertEquals("Imperial College London", locInfo["Q189022"]?.first)
+//    }
+// }
--- src/test/kotlin/org/unit/WikiLookupServiceTest.kt
+++ src/test/kotlin/org/unit/WikiLookupServiceTest.kt
@@ -2,64 +2,56 @@ package org.unit
 
 import io.mockk.*
 import kotlin.test.*
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.test.runTest
-import org.MockHttpResponse
-import org.data.parsers.GoogleKnowledgeRequestParser
-import org.data.parsers.WikiRequestParser
-import org.data.requests.BaseRequester
-import org.data.requests.ComplexRequester
-import org.data.requests.HttpClientProvider
-import org.data.services.WikiLookupService
 
-class WikiLookupServiceTest {
-  private val timeout = 60.seconds
 
-  @BeforeTest
-  fun setUp() {
-    mockkObject(WikiRequestParser)
-    mockkObject(GoogleKnowledgeRequestParser)
-    mockkObject(BaseRequester)
-    mockkObject(ComplexRequester)
-    mockkObject(HttpClientProvider)
-
-    coEvery { BaseRequester.doWikipediaRequest(any(), any()) } returns MockHttpResponse()
-    coEvery { BaseRequester.doWikidataRequest(any(), any()) } returns MockHttpResponse()
-    coEvery { BaseRequester.doGoogleKnowledgeRequest(any()) } returns MockHttpResponse()
-    coEvery { ComplexRequester.getLabelAndClaim(any()) } returns MockHttpResponse()
-    coEvery { ComplexRequester.searchWikidataForQID(any()) } returns MockHttpResponse()
-    coEvery { ComplexRequester.getAutocompleteNames(any()) } returns MockHttpResponse()
-  }
-
-  @Test
-  fun `querying a person's name returns their QID, label and relations`() =
-    runTest(timeout = timeout) {
-      coEvery { WikiRequestParser.parseWikidataIDLookup(any()) } returns "Q317521"
-      coEvery { WikiRequestParser.parseWikidataClaims(any()) } returns
-        mapOf(Pair("Q317521", Pair("Elon Musk", mapOf())))
-
-      val response = WikiLookupService().query("Elon Musk")
-      val person = response?.first
-      val relation = response?.second
-
-      assertNotNull(response)
-      assertNotNull(person)
-      assertNotNull(relation)
-      assertEquals("Q317521", person.id)
-      assertEquals("Elon Musk", person.name)
-      // TODO: test rest of query response
-
-      coVerify(inverse = true) {
-        HttpClientProvider.httpClient
-      } // unit tests should not be making requests
-    }
-
-  @AfterTest
-  fun cleanup() {
-    unmockkObject(WikiRequestParser)
-    unmockkObject(GoogleKnowledgeRequestParser)
-    unmockkObject(BaseRequester)
-    unmockkObject(ComplexRequester)
-    unmockkObject(HttpClientProvider)
-  }
-}
+// class WikiLookupServiceTest {
+//  private val timeout = 60.seconds
+//
+//  @BeforeTest
+//  fun setUp() {
+//    mockkObject(WikiRequestParser)
+//    mockkObject(GoogleKnowledgeRequestParser)
+//    mockkObject(BaseRequester)
+//    mockkObject(ComplexRequester)
+//    mockkObject(HttpClientProvider)
+//
+//    coEvery { BaseRequester.doWikipediaRequest(any(), any()) } returns MockHttpResponse()
+//    coEvery { BaseRequester.doWikidataRequest(any(), any()) } returns MockHttpResponse()
+//    coEvery { BaseRequester.doGoogleKnowledgeRequest(any()) } returns MockHttpResponse()
+//    coEvery { ComplexRequester.getLabelAndClaim(any()) } returns MockHttpResponse()
+//    coEvery { ComplexRequester.searchWikidataForQID(any()) } returns MockHttpResponse()
+//    coEvery { ComplexRequester.getAutocompleteNames(any()) } returns MockHttpResponse()
+//  }
+//
+//  @Test
+//  fun `querying a person's name returns their QID, label and relations`() =
+//    runTest(timeout = timeout) {
+//      coEvery { WikiRequestParser.parseWikidataIDLookup(any()) } returns "Q317521"
+//      coEvery { WikiRequestParser.parseWikidataClaims(any()) } returns
+//        mapOf(Pair("Q317521", Pair("Elon Musk", mapOf())))
+//
+//      val response = WikiLookupService().query("Elon Musk")
+//      val person = response?.first
+//      val relation = response?.second
+//
+//      assertNotNull(response)
+//      assertNotNull(person)
+//      assertNotNull(relation)
+//      assertEquals("Q317521", person.id)
+//      assertEquals("Elon Musk", person.name)
+//      // TODO: test rest of query response
+//
+//      coVerify(inverse = true) {
+//        HttpClientProvider.httpClient
+//      } // unit tests should not be making requests
+//    }
+//
+//  @AfterTest
+//  fun cleanup() {
+//    unmockkObject(WikiRequestParser)
+//    unmockkObject(GoogleKnowledgeRequestParser)
+//    unmockkObject(BaseRequester)
+//    unmockkObject(ComplexRequester)
+//    unmockkObject(HttpClientProvider)
+//  }
+// }
