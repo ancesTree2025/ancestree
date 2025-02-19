0f470b60e49bcbe42079207b5b485aca1c80d5ad
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-19 15:33:53 +0000

    fix(frontend): dropdown menu does not re-trigger search (#31)
    Co-authored-by: jamacz <jack.mccomish@icloud.com>

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
   import { page } from '$app/state';
 
-  let name = $state<string | undefined>();
+  const name = $state<string | undefined>();
   let status = $state<LoadingStatus>({ state: 'idle' });
 
   let tree = $state<Tree | undefined>();
@@ -36,6 +36,20 @@
     }
   });
 
+  async function onSubmit(name: string) {
+    if (!name.length) return;
+
+    status = { state: 'loading' };
+    const result = await fetchTree(name, useFakeData);
+    const [fetched, error] = result.toTuple();
+    if (fetched) {
+      tree = fetched;
+      status = { state: 'idle' };
+    } else if (error) {
+      status = { state: 'error', error };
+    }
+  }
+
   let showSidePanel = $state(false);
   let sidePanelName = $state<string | undefined>(undefined);
   let sidePanelData = $state<PersonInfo | undefined>(undefined);
@@ -62,7 +76,7 @@
   <nav class="flex items-center gap-12 px-8 py-4 shadow-lg">
     <h1 class="w-48 text-lg">Ancestree</h1>
     <div class="flex flex-1 justify-center">
-      <NameInput bind:nameInput={name} {status} />
+      <NameInput {onSubmit} {status} />
     </div>
     <div class="w-48"></div>
   </nav>
@@ -78,6 +92,3 @@
     />
   </div>
 </div>
-
-<style scoped>
-</style>
