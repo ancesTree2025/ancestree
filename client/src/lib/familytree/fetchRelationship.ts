6da7fed73a9b3ae8508ff6cc829ee7121528e39a
s-denisov <67757505+s-denisov@users.noreply.github.com>
2025-02-21 13:19:16 +0000

    feat(frontend): allow searching for relationship (#38)
    Co-authored-by: Arsenii Denisov <simon.denisov22@gmail.com>

---
+++ client/src/lib/familytree/fetchRelationship.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+import { personIdSchema } from './fetchTree';
+import exampleRelationship from '../data/exampleRelationship.json';
+import { Result } from 'typescript-result';
+import type { PersonID } from './types';
+
+const relationshipSchema = z.object({
+  relation: z.string(),
+  links: z.array(personIdSchema)
+});
+
+export type Relationship = z.infer<typeof relationshipSchema>;
+
+export async function fetchRelationship(
+  from: PersonID,
+  to: PersonID,
+  useFakeData: boolean
+): Promise<Result<Relationship, string>> {
+  if (useFakeData) {
+    if (from === 'F' && to === 'GC') {
+      return Result.ok({
+        ...exampleRelationship,
+        links: [from].concat(exampleRelationship.links)
+      });
+    }
+    return Result.error('Only have sample data for qid1=D&qid2=C3');
+  }
+  const response = await Result.fromAsyncCatching(
+    fetch(`http://localhost:8080/relation?orig=${from}&dest=${to}`)
+  ).mapError(() => 'Could not connect to server');
+  if (response.getOrNull()?.status === 404) {
+    return Result.error('Person not found');
+  }
+  const parsed = response.mapCatching(
+    async (response) => (await response.json()) as Relationship,
+    () => 'Could not parse server response'
+  );
+  return parsed.mapCatching(
+    (json) => {
+      const relationship = relationshipSchema.parse(json);
+      return Result.ok({
+        ...relationship,
+        links: [from].concat(relationship.links)
+      });
+    },
+    () => 'Server data in wrong format'
+  );
+}
