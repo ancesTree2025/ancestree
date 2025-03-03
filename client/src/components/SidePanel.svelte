1a9d83c94591f3ccb66e0fe3779bbf5ee87040d1
s-denisov <67757505+s-denisov@users.noreply.github.com>
2025-03-03 16:18:22 +0000

    feat(frontend): make sidebar display permanently (#48)
    Co-authored-by: Arsenii Denisov <simon.denisov22@gmail.com>

--- client/src/components/SidePanel.svelte
+++ client/src/components/SidePanel.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
   import type { PersonInfo } from '$lib/types';
-  import IconClose from '~icons/tabler/x';
 
   interface Props {
     name?: string;
     data?: PersonInfo;
     show: boolean;
-    onclose: () => void;
   }
-  const { name, data, show, onclose }: Props = $props();
+  const { name, data, show }: Props = $props();
 </script>
 
 <div class={`${show ? 'w-96' : 'w-0'} shadow-lg transition-all duration-500`}>
   <div class="flex w-96 flex-col gap-6 px-8 py-6">
     <div class="flex items-center justify-between">
-      <div class="text-xl font-semibold">{name}</div>
-      <button class="cursor-pointer p-4" onclick={onclose}>
-        <IconClose />
-      </button>
+      {#if data}
+        <div class="text-xl font-semibold">{name}</div>
+      {:else}
+        <div class="h-8 w-3/4 animate-pulse rounded-lg bg-gray"></div>
+      {/if}
     </div>
     {#if data}
       <img alt={name} class="mx-8 mb-4 aspect-square rounded-xl object-cover" src={data.image} />
@@ -30,6 +29,15 @@
       <p class="mt-4">
         {data.description} <a href={data.wikipedia_link}>Wikipedia</a>
       </p>
+    {:else}
+      <img
+        alt={name}
+        class="mx-8 mb-4 aspect-square rounded-xl object-cover"
+        src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+      />
+      <div class="h-4 w-3/4 animate-pulse rounded-lg bg-gray"></div>
+      <div class="h-4 w-1/2 animate-pulse rounded-lg bg-gray"></div>
+      <div class="w-100 h-40 animate-pulse rounded-lg bg-gray"></div>
     {/if}
   </div>
 </div>
