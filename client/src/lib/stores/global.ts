fa1db4e4d2c86999b1ed1a9064a42f649ec73314
mozin751 <123524914+mozin751@users.noreply.github.com>
2025-03-03 17:25:03 +0000

    prettier run

--- client/src/lib/stores/global.ts
+++ client/src/lib/stores/global.ts
@@ -1,10 +1,9 @@
 import type { InfoChecklist } from '$lib/types';
 
-
 export const checkboxOptions: InfoChecklist = [
-	{ key: 'image', label: 'Show Image', checked: true },
-	{ key: 'birth', label: 'Show Birth Date', checked: true },
-	{ key: 'death', label: 'Show Death Date', checked: true },
-	{ key: 'description', label: 'Show Description', checked: true },
-	{ key: 'wikiLink', label: 'Show Wikipedia Link', checked: true }
+  { key: 'image', label: 'Show Image', checked: true },
+  { key: 'birth', label: 'Show Birth Date', checked: true },
+  { key: 'death', label: 'Show Death Date', checked: true },
+  { key: 'description', label: 'Show Description', checked: true },
+  { key: 'wikiLink', label: 'Show Wikipedia Link', checked: true }
 ];
