2694b1afdd85d4d72a3b16c8c40f5a23e51ed4e7
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-03-05 17:49:25 +0000

    don't show if coord string is Unknown

--- client/src/components/SidePanel.svelte
+++ client/src/components/SidePanel.svelte
@@ -36,7 +36,7 @@
       {/each}
       {#if data.bcoords || data.dcoords}
         <div class="mt-4 space-y-4">
-          {#if data.bcoords}
+          {#if data.bcoords || data.bcoords === 'Unknown'}
             <div>
               <p><strong>Birthplace:</strong></p>
               <a
@@ -58,7 +58,7 @@
             </div>
           {/if}
 
-          {#if data.dcoords}
+          {#if data.dcoords || data.dcoords === 'Unknown'}
             <div>
               <p><strong>Death Place:</strong></p>
               <a
