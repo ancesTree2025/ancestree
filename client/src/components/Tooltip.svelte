4217a0528fd620aa513ed922010657e5bcc775f0
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-03-07 00:44:41 +0000

    feat(frontend): advanced tooltip placement (#71)

--- client/src/components/Tooltip.svelte
+++ client/src/components/Tooltip.svelte
@@ -1,20 +1,60 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
 
+  type Vertical = 't' | 'm' | 'b';
+  type Horizontal = 'l' | 'm' | 'r';
+  type Position = `${Vertical}${Horizontal}`;
+
   type Props = {
     title: string;
     children: Snippet<[]>;
-    position?: 'top' | 'bottom';
+    position?: Position;
   };
 
-  const { title, children, position = 'top' }: Props = $props();
+  const { title, children, position = 'mm' }: Props = $props();
+
+  function positionClass(): string {
+    const clazz = [];
+    const vertical = position.charAt(0) as Vertical;
+    const horizontal = position.charAt(1) as Horizontal;
+
+    switch (vertical) {
+      case 't':
+        clazz.push('bottom-full');
+        break;
+
+      case 'm':
+        clazz.push('top-1/2 -translate-y-1/2');
+        break;
+
+      case 'b':
+        clazz.push('top-full');
+        break;
+    }
+
+    switch (horizontal) {
+      case 'l':
+        clazz.push('right-1/2');
+        break;
+
+      case 'm':
+        clazz.push('left-1/2 -translate-x-1/2');
+        break;
+
+      case 'r':
+        clazz.push('left-1/2');
+        break;
+    }
+
+    return clazz.join(' ');
+  }
 </script>
 
 <div class="group relative">
   {@render children()}
 
   <div
-    class={`absolute ${position === 'top' ? 'bottom-full' : 'top-full'} left-1/2 w-full min-w-max -translate-x-1/2 rounded-md bg-black/90 p-2 text-sm text-white opacity-0 transition-opacity group-hover:opacity-100`}
+    class={`${positionClass()} absolute w-full min-w-max rounded-md bg-black/90 p-2 text-sm text-white opacity-0 transition-opacity group-hover:opacity-100`}
   >
     {title}
   </div>
